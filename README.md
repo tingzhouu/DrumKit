2d7cc69b7f88d9358a20ca0e35309d6e30cf82f0
Ting Zhou <limtingzhou@gmail.com>
2018-12-16 21:23:01 +0800

    Add functionality to play sound from keyboard key and button click
    color of text changes when drum is 'hit'. 
    event listener for button click
    event listener for specific keyboard keys.

---
+++ README.md
@@ -0,0 +1,17 @@
+# DrumKit
+
+This repository serves to document my learning of using Javascript in a webpage to modify the Document Object Model (DOM) via a tutorial to create a website that can play drums from keyboard press or mouse click on button.
+
+## What I Have Used
+
+**HTML CSS Javscript**
+Event Listeners for button click and keyboard press
+
+## Getting Started
+Click on link: https://tingzhouu.github.com/Dicee
+Once page is loaded, refresh the page to start rolling!
+
+
+## Authors
+
+* **Ting Zhou** - [GitHub Link](https://github.com/tingzhouu)
---
+++ index.js
@@ -0,0 +1,77 @@
+let drums = document.querySelectorAll(".drum");
+console.log(drums);
+drums.forEach(function(drum) {
+  drum.addEventListener("click", handleClick);
+});
+
+//Detecting button press
+function handleClick() {
+  changeButtonColor(this);
+  let letter = this.innerHTML;
+  playAudio(letter);
+}
+
+//Detecting keyboard press
+function handleKey(e) {
+  let letter = e.key;
+  let button = document.querySelector("." + letter);
+  changeButtonColor(button);
+  playAudio(letter);
+}
+
+//Changing Color Upon Keyboard Press/Button Click
+function changeButtonColor(button) {
+  if (button != null) {
+    let color = getComputedStyle(button).getPropertyValue('color');
+
+    if (color === "rgb(218, 4, 99)") {
+      button.style.color = "#393424";
+
+    } else {
+      button.style.color = "rgb(218, 4, 99)";
+    }
+  }
+}
+
+document.addEventListener("keypress", handleKey);
+
+function playAudio(letter) {
+  let audio = null;
+
+  switch (letter) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      break;
+
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+      break;
+
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+
+    default:
+      console.log(letter);
+  }
+
+  if (audio != null) {
+    audio.play();
+  }
+}
